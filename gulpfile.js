efc4f0000b99faaf78b95414af333f8bfcfee15b
brandon cooper and kory skarbek <brandon@>
2017-03-13 09:53:14 -0700

    ping pong

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('myTask', function(){
+  console.log('hello gulp');
+});
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+gulp.task('concatInterface', function() {
+  // return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+  .pipe(uglify())
+  .pipe(gulp.dest("./build/js"));
+});
